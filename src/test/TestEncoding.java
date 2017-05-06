6b1c289e9e837970e8b1fddbf935f2159108c02b
yzj <yuzhujun14@hotmail.com>
2017-05-06 17:43:05 +0800

    下载文件测试完毕，中文编码有问题，目前客户端用gbk解码可以解决，但未测试跨平台性能

---
+++ src/test/TestEncoding.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.io.UnsupportedEncodingException;
+
+public class TestEncoding {
+	public static void main (String args[]) throws UnsupportedEncodingException{
+		String s="ÖÐÎÄ";
+		byte[] buffer1=s.getBytes("UTF-8");
+		byte[] buffer2=s.getBytes("GBK");
+ 		String s1=new String(buffer1,"UTF-8");
+		String s2=new String(buffer2,"UTF-8");
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
